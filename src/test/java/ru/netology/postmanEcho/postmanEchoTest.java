333a9f08552b0cb8d42a047b9c851460900f9118
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-08-07 20:18:43 +0400

    refactor test

--- src/test/java/ru/netology/postmanEcho/postmanEchoTest.java
+++ src/test/java/ru/netology/postmanEcho/postmanEchoTest.java
@@ -1,23 +1,28 @@
 package ru.netology.postmanEcho;
 
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 class postmanEchoTest {
     @Test
-    void postRequestTest() {
+    void testBodySomeData() {
         // Given - When - Then
-        // Предусловия
+// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-        // Выполняемые действия
+                .body( "some data" ) // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
                 .when()
                 .post("/post")
-        // Проверки
+// Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", Matchers.equalTo("some data"))
         ;
     }
 }
